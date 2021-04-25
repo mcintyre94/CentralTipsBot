184ddebe7200ad19dd5cf097dd8574c8e05e65f9
Callum McIntyre <callum.mcintyre@procensus.com>
2021-04-25 12:16:44 +0100

    Add code to use the Twitter API to get the latest relevant Tweets
    - Uses the v2 API with bearer token
    - Match tweets tagging the bot in a reply, so we get in_reply_to
    - Recurse for pagination

--- config/config.exs
+++ config/config.exs
@@ -40,7 +40,8 @@ config :extwitter, :oauth, [
   consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
   consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
   access_token: System.get_env("TWITTER_ACCESS_TOKEN"),
-  access_token_secret: System.get_env("TWITTER_ACCESS_SECRET")
+  access_token_secret: System.get_env("TWITTER_ACCESS_SECRET"),
+  bearer_token: System.get_env("TWITTER_BEARER_TOKEN")
 ]
 
 # Import environment specific config. This must remain at the bottom
--- lib/centraltipsbot/twitter.ex
+++ lib/centraltipsbot/twitter.ex
@@ -1,6 +1,8 @@
 defmodule Centraltipsbot.Twitter do
   require Logger
 
+  @bearer_token Application.get_env(:extwitter, :oauth)[:bearer_token]
+
   # Paginated API, this function makes a single call to
   # https://developer.twitter.com/en/docs/twitter-api/v1/direct-messages/sending-and-receiving/api-reference/list-events
   defp fetch_next_dms(cursor) do
@@ -53,6 +55,75 @@ defmodule Centraltipsbot.Twitter do
       e ->
         # Sending read receipts is non-critical, so just ignore any errors
         Logger.info("Error when trying to mark DMs read: #{inspect(e)}")
+    end
+  end
+
+  # Paginated API, this function makes a single call to
+  # https://developer.twitter.com/en/docs/twitter-api/tweets/search/api-reference/get-tweets-search-recent
+  # Using a bearer token as authorization
+  defp fetch_next_tweets(since_id, next_token) do
+    url = ExTwitter.API.Base.request_url("2/tweets/search/recent")
+
+    params = [
+      # Match replies tagging the bot and exclude retweets
+      query: "@CentralTipsBot is:reply -is:retweet",
+      max_results: 100,
+      "tweet.fields": "author_id,in_reply_to_user_id,created_at"
+    ] ++ case since_id do
+      nil -> []
+      _ -> [since_id: since_id]
+    end ++ case next_token do
+      nil -> []
+      _ -> [next_token: next_token]
+    end
+    |> ExTwitter.Parser.parse_request_params
+    |> URI.encode_query
+
+    url="#{url}?#{params}"
+    headers = ["Authorization": "Bearer #{@bearer_token}"]
+
+    {:ok, response} = HTTPoison.get(url, headers)
+    case response.status_code do
+      200 -> {:ok, response.body |> Jason.decode!}
+      429 ->
+        # Rate limited
+        rate_limit_reset = response.headers |> Map.new |> Map.get("x-rate-limit-reset") |> String.to_integer
+        {:rate_limit, rate_limit_reset}
+      _ -> {:err, response}
+    end
+  end
+
+  # Request DMs since the given cursor, which represents the last request we made
+  # Returns newest first
+  def tweets_to_process_since_id(since_id, next_token \\ nil, tweets_so_far \\ []) do
+    case fetch_next_tweets(since_id, next_token) do
+      {:ok, response} ->
+        meta = Map.get(response, "meta")
+        data = Map.get(response, "data", nil) # data not returned when no tweets
+
+        tweets_so_far = case data do
+          nil -> tweets_so_far
+          _ -> [tweets_so_far | data] # We can always append because later results (using next_token) always get older tweets in order
+        end
+
+        case Map.get(meta, "next_token") do
+          nil ->
+            # No further pages, return what we got
+            tweets_so_far |> List.flatten
+          next_token ->
+            # Further pages, request more
+            Logger.info("Fetching more Tweets, since_id: #{since_id}, next_token: #{next_token}")
+            tweets_to_process_since_id(since_id, next_token, tweets_so_far)
+        end
+      {:rate_limit, rate_limit_reset} ->
+        # Got a Twitter rate limit, sleep until we can request more
+        now = :os.system_time(:second)
+        sleep_for_seconds = rate_limit_reset - now + 1
+        Logger.info("Got Twitter rate limit, waiting #{sleep_for_seconds} seconds before re-trying")
+        :timer.sleep (sleep_for_seconds * 1000)
+        tweets_to_process_since_id(since_id, next_token, tweets_so_far)
+      {:err, response} ->
+        Logger.error("Received unexpected response from Twitter API: #{inspect(response)}")
     end
   end
 end
